ab7648dde7d82e0613d9db1388871253221bff7c
Rümeysa Tokgöz <126255709+tokgozr@users.noreply.github.com>
2024-08-18 13:53:26 +0300

    Create reverser.cpp

---
+++ reverser.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+int main()
+{
+    string input;
+    cout << "Bir kelime veya cumle giriniz: \n";
+    getline(cin,input);
+
+    string reversed;
+    for(int i = input.length()-1; i>=0;i--)
+    {
+        reversed += input[i];
+    }
+    cout << "Tersine cevrilmis hali: " << reversed << endl;
+}
